d64b9da468debbd9f6475d13cef7f71b09f92691
buhtig <78307869+pythonnoob3000@users.noreply.github.com>
2023-05-15 19:48:04 +0800

    Add files via upload

---
+++ Game/Pawn.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "..\ChessPiece.h"
+class Pawn :
+    public ChessPiece
+{
+protected:
+    const Vector2i moveDirs[2] =
+    {
+        Vector2i(0,  1),
+        Vector2i(0,  -1),
+    };
+    bool hasMoved = false; 
+    void GeneratePossibleMoves();
+public:
+    Pawn(GameBoard* board, const Vector2i& position, const Team& team);
+};
+
